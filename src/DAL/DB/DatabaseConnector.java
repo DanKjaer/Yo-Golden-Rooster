4b1ef18d09b3306a9a56c821c8672b4fe137f9a7
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2023-01-03 11:10:06 +0100

    Create DB Connector
    Still need to add libaries to connect

---
+++ src/DAL/DB/DatabaseConnector.java
@@ -0,0 +1,39 @@
+package DAL.DB;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+//Class will easv.mrs.be included when we start working on DATABASES
+
+    private SQLServerDataSource dataSource;
+
+    public MyDatabaseConnector()
+    {
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName("10.176.111.31");
+        dataSource.setDatabaseName("NetflixRecommendationSystem");
+        dataSource.setUser("CSe22A_40");
+        dataSource.setPassword("CSe22A_40");
+        dataSource.setPortNumber(1433);
+    }
+
+    public Connection getConnection() throws SQLServerException {
+        return dataSource.getConnection();
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+
+        MyDatabaseConnector databaseConnector = new MyDatabaseConnector();
+
+        try (Connection connection = databaseConnector.getConnection()) {
+
+            System.out.println("Is it open? " + !connection.isClosed());
+
+        } //Connection gets closed here
+    }
+}
