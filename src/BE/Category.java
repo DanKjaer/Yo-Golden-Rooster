f2dc2a1ddcccf20d4938ecbf0de8f4051b4c71c4
ShadySloth <pedant01@easv365.dk>
2023-01-03 11:08:16 +0100

    BE

---
+++ src/BE/Category.java
@@ -0,0 +1,6 @@
+package BE;
+
+public class Category {
+    private int Id;
+    private String Name;
+}
--- src/BE/Movie.java
+++ src/BE/Movie.java
@@ -1,4 +1,11 @@
 package BE;
 
+import java.util.Date;
+
 public class Movie {
+    private int Id;
+    private String Name;
+    private double Rating;
+    private String Filelink;
+    private Date Lastview;
 }
