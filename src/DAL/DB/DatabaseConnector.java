1f17efbb489426ecd40b0a9b6ad7e133624ebc52
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2023-01-03 11:20:48 +0100

    Connector and Library done and importet

--- src/DAL/DB/DatabaseConnector.java
+++ src/DAL/DB/DatabaseConnector.java
@@ -7,28 +7,26 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DatabaseConnector {
-//Class will easv.mrs.be included when we start working on DATABASES
-
     private SQLServerDataSource dataSource;
 
-    public MyDatabaseConnector()
+    public DatabaseConnector()
     {
         dataSource = new SQLServerDataSource();
         dataSource.setServerName("10.176.111.31");
-        dataSource.setDatabaseName("NetflixRecommendationSystem");
-        dataSource.setUser("CSe22A_40");
-        dataSource.setPassword("CSe22A_40");
+        dataSource.setDatabaseName("PMC_Yo_Golden_Rooster");
+        dataSource.setUser("CSe22A_21");
+        dataSource.setPassword("CSe22A_21");
         dataSource.setPortNumber(1433);
+        dataSource.setTrustServerCertificate(true);
     }
 
     public Connection getConnection() throws SQLServerException {
         return dataSource.getConnection();
     }
 
-
     public static void main(String[] args) throws SQLException {
 
-        MyDatabaseConnector databaseConnector = new MyDatabaseConnector();
+        DatabaseConnector databaseConnector = new DatabaseConnector();
 
         try (Connection connection = databaseConnector.getConnection()) {
 
