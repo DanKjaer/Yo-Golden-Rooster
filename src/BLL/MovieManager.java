bbc7f1e3797bc26fa47a7a73dc9f37bcb8a5befa
ShadySloth <pedant01@easv365.dk>
2023-01-04 11:26:38 +0100

    oof

--- src/BLL/MovieManager.java
+++ src/BLL/MovieManager.java
@@ -1,27 +1,16 @@
 package BLL;
-
-
-import BE.Movie;
-
-public class MovieManager {
-    public Movie getMovies() {
-        return null;
-
 import BE.Category;
 import BE.Movie;
 import DAL.DB.CategoryDAO;
 import DAL.DB.ICategoryDatabaseAccess;
 import DAL.DB.IMovieDatabaseAccess;
-
 import java.util.List;
-
 public class MovieManager {
+
+
+
     private ICategoryDatabaseAccess categoryDAO;
     private IMovieDatabaseAccess movieDAO;
-
-
-
-
 
     public List<Movie> getMovies() throws Exception{
         return movieDAO.getMovies();
