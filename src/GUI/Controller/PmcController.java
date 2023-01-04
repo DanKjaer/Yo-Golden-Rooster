331cfc660aca72bf4f28eace180b5a911e3afe12
ShadySloth <pedant01@easv365.dk>
2023-01-04 09:54:30 +0100

    btns and shit

--- src/GUI/Controller/PmcController.java
+++ src/GUI/Controller/PmcController.java
@@ -1,4 +1,49 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.text.Text;
+
 public class PmcController {
+    public Button btnAdd;
+    public Button btnDelete;
+    public Button btnCategory;
+    public TextField tfSearch;
+    public TableView lstMovie;
+    public TableColumn clnTitle;
+    public TableColumn clnCategory;
+    public TableColumn clnPersonalRating;
+    public TableColumn clnLastView;
+    public Button btnPlay;
+    public TextField tfRating;
+    public Button btnRate;
+    public ImageView imgMovie;
+    public Text txtTitle;
+    public Text txtRating;
+    public Text txtPersonalRating;
+    public Text txtCategory;
+    public Text txtLastView;
+
+    public void handleAdd(ActionEvent actionEvent) {
+    }
+
+    public void handleDelete(ActionEvent actionEvent) {
+    }
+
+    public void handleEditCategory(ActionEvent actionEvent) {
+    }
+
+    public void handleSearch(InputMethodEvent inputMethodEvent) {
+    }
+
+    public void handlePlay(ActionEvent actionEvent) {
+    }
+
+    public void handleRate(ActionEvent actionEvent) {
+    }
 }
