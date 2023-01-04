791638ce17d43ec30403173c80add95a7d9f7d20
ShadySloth <pedant01@easv365.dk>
2023-01-04 09:42:02 +0100

    GUI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
+import GUI.Controller.PmcController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("GUI/View/PmcView.fxml"));
+        Parent root = loader.load();
+
+        primaryStage.setTitle("Private Movie Collection");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
