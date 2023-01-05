3df672bba711ffa99de8731520ede72abd9cd94d
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2023-01-05 08:44:39 +0100

    Tilføjet contructor på MovieDAO

--- src/DAL/DB/IMovieDatabaseAccess.java
+++ src/DAL/DB/IMovieDatabaseAccess.java
@@ -1,13 +1,13 @@
 package DAL.DB;
 
 import BE.Movie;
-import com.microsoft.sqlserver.jdbc.SQLServerException;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IMovieDatabaseAccess {
 
-    public List<Movie> getMovies() throws SQLServerException;
+    public List<Movie> getMovies() throws SQLException;
 
     public Movie createMovie(String name, String fileLink) throws Exception;
 
