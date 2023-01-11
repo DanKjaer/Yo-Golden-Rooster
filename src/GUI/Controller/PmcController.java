8c9807c9d281bdb28944c00f5a24eee00870ca05
ShadySloth <pedant01@easv365.dk>
2023-01-11 10:12:18 +0100

    loads of shit

--- src/GUI/Controller/PmcController.java
+++ src/GUI/Controller/PmcController.java
@@ -18,9 +18,13 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.util.*;
 import java.util.List;
 
@@ -70,7 +74,8 @@ public class PmcController extends BaseController {
     }
 
     /**
-     * Checks the DB when the program starts for old movies with less than 6.0 rating
+     * Checks the DB when the program starts for old movies with less than 6.0 rating,
+     * and prepares a string to be show to the user if any is found.
      */
     private void checkOldMovie(List<Movie> list) {
         StringBuilder sb = new StringBuilder();
@@ -114,7 +119,7 @@ public class PmcController extends BaseController {
     }
 
     /**
-     * Displays an alert if detectOldMovie is true
+     * Displays an alert if detectOldMovie is true with the old movies listed.
      */
     private void alertOldMovie() {
         if (detectOldMovie) {
@@ -221,16 +226,16 @@ public class PmcController extends BaseController {
     }
 
     /**
-     * A button used to rate the selected movie.
+     * Rates the selected movie.
      */
     @FXML
     private void handleRate() {
         try {
-            //Selects a movie. Parses the textfield from a string to a float, so you can add numbers.
+            //Selects a movie. Parses the textfield from a string to a float.
             Movie selectedMovie = (Movie) lstMovie.getSelectionModel().getSelectedItem();
             float rating = Float.parseFloat(tfRating.getText());
 
-            //Checks if the value is between 1 and 10, if it is, it'll update the movie list.
+            //Checks if the value is between 1 and 10, then updates the movie with the rating and the list of movies.
             if (rating >= 1 && rating <= 10) {
                 pmcModel.rateMovie(selectedMovie, rating);
                 updateMovieList();
@@ -258,7 +263,7 @@ public class PmcController extends BaseController {
     private void updateMovieList() {
         try {
             //Gives every column a property to look for in given object
-            // It uses the get"X" from the object, to retrieve the values
+            // It uses the getters from the object, to retrieve the values
             clnTitle.setCellValueFactory(new PropertyValueFactory<>("name"));
             clnCategory.setCellValueFactory(new PropertyValueFactory<>("categories"));
             clnPersonalRating.setCellValueFactory(new PropertyValueFactory<>("rating"));
@@ -281,7 +286,7 @@ public class PmcController extends BaseController {
             //Opens an alert box
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
             alert.setTitle("Delete that shit yo");
-            alert.setHeaderText("Delete movie");
+            alert.setHeaderText("You are about to delete a movie");
             alert.setContentText("Continue?");
             Optional<ButtonType> result = alert.showAndWait();
 
@@ -301,31 +306,35 @@ public class PmcController extends BaseController {
      * @param mouseEvent
      * @throws IOException
      */
-    public void onClickMovie(MouseEvent mouseEvent) throws IOException {
-        //Gets the selected movie and scrapes imdb page for their rating and poster
-        Movie selectedMovie = (Movie) lstMovie.getSelectionModel().getSelectedItem();
-        //String URL = selectedMovie;
-        //Document doc = Jsoup.connect(URL).get();
-        //String rating = doc.select("span.sc-7ab21ed2-1.eUYAaq").text();
-        //String poster = doc.select("img.ipc-image").attr("src");
+    public void onClickMovie(MouseEvent mouseEvent) {
+        try {
+            //Gets the selected movie and scrapes imdb page for their rating and poster
+            Movie selectedMovie = (Movie) lstMovie.getSelectionModel().getSelectedItem();
+            String URL = selectedMovie.getWebsite();
+            Document doc = Jsoup.connect(URL).get();
+            String rating = doc.select("span.sc-7ab21ed2-1.eUYAaq").text();
+            String poster = doc.select("img.ipc-image").attr("src");
 
-        //Adds all the additional info
-        txtTitle.setText(selectedMovie.getName());
-        txtPersonalRating.setText(String.valueOf(selectedMovie.getRating()));
-        if (selectedMovie.getLastview() != null) {
+            //Adds all the additional info
+            txtTitle.setText(selectedMovie.getName());
+            txtPersonalRating.setText(String.valueOf(selectedMovie.getRating()));
             txtLastView.setText(String.valueOf(selectedMovie.getLastview()));
-        } else {
-            txtLastView.setText("Never seen");
-        }
-        txtCategory.setText(selectedMovie.getCategories());
+            txtCategory.setText(selectedMovie.getCategories());
+            txtRating.setText(rating.substring(0,3));
+            imgMovie.setImage(new Image(poster));
 
-        btnRate.setDisable(false);
-        btnPlay.setDisable(false);
-        btnDelete.setDisable(false);
+            //Enables the rate, play and delete buttons
+            btnRate.setDisable(false);
+            btnPlay.setDisable(false);
+            btnDelete.setDisable(false);
+        } catch (IOException e) {
+            displayError(e);
+            e.printStackTrace();
+        }
     }
 
     /**
-     * Enables the search function, making you able to search for either the title, category or rating.
+     * Enables the search function, making you able to search for either the title, category or rating in real time.
      */
     private void search(){
         //Adds a listener to our search bar, making it able to update in real time.
@@ -338,5 +347,19 @@ public class PmcController extends BaseController {
                 e.printStackTrace();
             }
         });
+    }
+
+    /**
+     * Opens the url of the movie on your standard browser.
+     * @param actionEvent
+     */
+    public void handleImdb(ActionEvent actionEvent) {
+        try {
+            Movie selectedMovie = (Movie) lstMovie.getSelectionModel().getSelectedItem();
+            Desktop.getDesktop().browse(URI.create(selectedMovie.getWebsite()));
+        } catch (IOException e) {
+            displayError(e);
+            e.printStackTrace();
+        }
     }
 }
